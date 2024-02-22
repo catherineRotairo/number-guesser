57ceb819808821595085080885d5b2a65ea141c3
catherineRotairo <catherinerotairo@Catherines-MacBook-Pro.local>
2024-02-23 10:14:13 +1300

    Add functionality to script.js

--- script.js
+++ script.js
@@ -2,5 +2,24 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 10);
 
+const compareGuesses = (humanGuess, computerGuess, targetNum) => {
+    let humanG = Math.abs(targetNum - humanGuess);
+    let computerG = Math.abs(targetNum - computerGuess);
+    return humanG <= computerG;
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    } else {
+        console.log('invalid winner input');
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++
+}
